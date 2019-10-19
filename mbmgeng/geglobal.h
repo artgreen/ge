cbdb8a01d61dc03fbf6b754b7fca7c20673889e4
green <arthur@greenjeans.net>
2019-10-19 12:43:32 -0500

    Reformatted code

--- mbmgeng/geglobal.h
+++ mbmgeng/geglobal.h
@@ -11,208 +11,203 @@
  *                                                                         *
  ***************************************************************************/
 
- /**************************************************************************
-  * This program is free software; you can redistribute it and/or modify   *
-  * under the terms of the GNU General Public License as published by the  *
-  * Free Software Foundation; either version 2 of the License, or (at your *
-  * option) any later version.                                             *
-  *                                                                        *
-  * This program is distributed in the hope that it will be useful,        *
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
-  * General Public License for more details.                               *
-  *                                                                        *
-  * You should have received a copy of the GNU General Public License      *
-  * along with this program; if not, write to the Free Software Foundation,*
-  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA            *
-  *************************************************************************/
+/**************************************************************************
+ * This program is free software; you can redistribute it and/or modify   *
+ * under the terms of the GNU General Public License as published by the  *
+ * Free Software Foundation; either version 2 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This program is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU General Public License      *
+ * along with this program; if not, write to the Free Software Foundation,*
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA            *
+ *************************************************************************/
 
- /**************************************************************************
- * Authors Note: The source code to this program is being made available   *
- *               to the general public in the hopes that it will continue  *
- *               to live on and evolve. While I cannot provide the time    *
- *               to personally participate in its development any longer,  *
- *               I am eager to assist in the design discussions, testing   *
- *               and project discussions.                                  *
- *                                                                         *
- *               To foster a community for Galactic Empire developers I    *
- *               have created a project home page at...                    *
- *               http://mbbs.sitesage.net                                  *
- *                                                                         *
- *               I may be contacted via email at mmurdock@starphire.com    *
- ***************************************************************************/
- 
-/* NOTE: I like my tabs at 3 so if the code looks "messed up" try changing */
-/*       your tabs to 3....    MBM                                         */
-
+/**************************************************************************
+* Authors Note: The source code to this program is being made available   *
+*               to the general public in the hopes that it will continue  *
+*               to live on and evolve. While I cannot provide the time    *
+*               to personally participate in its development any longer,  *
+*               I am eager to assist in the design discussions, testing   *
+*               and project discussions.                                  *
+*                                                                         *
+*               To foster a community for Galactic Empire developers I    *
+*               have created a project home page at...                    *
+*               http://mbbs.sitesage.net                                  *
+*                                                                         *
+*               I may be contacted via email at mmurdock@starphire.com    *
+***************************************************************************/
 
 /* GEMAIN Global Defs ****************************************************/
 
 #ifndef GEMAIN
 
-extern  int			gestt;
+extern int gestt;
 
-extern  BTVFILE	*gebb1,  /* GEship.dat   */
-						*gebb2,  /* GEplanet.dat */
-						*gebb4,  /* GEmail.dat   */
-						*gebb5;  /* GEuser.dat   */
+extern BTVFILE *gebb1,  /* GEship.dat   */
+        *gebb2,  /* GEplanet.dat */
+        *gebb4,  /* GEmail.dat   */
+        *gebb5;  /* GEuser.dat   */
 
-extern  FILE		*gemb, *gehlpmb, *geshmb;
-extern  FILE		*opnmsg();
+extern FILE *gemb, *gehlpmb, *geshmb;
 
-extern  int			numwar;                 /* number of users in game  */
+extern FILE *opnmsg();
 
-extern  WARSHP		tmpshp;  /* used to temporarly set up a new ship */
-extern  WARSHP		*warshp,*warsptr;
-extern  WARUSR		*warusr,*waruptr;
-extern  int			warusr_ecl,warshp_ecl;
-extern  WARUSR		tmpusr;
+extern int numwar;                 /* number of users in game  */
 
-extern  GALSECT	sector,*sptr1;
+extern WARSHP tmpshp;  /* used to temporarly set up a new ship */
+extern WARSHP *warshp, *warsptr;
+extern WARUSR *warusr, *waruptr;
+extern int warusr_ecl, warshp_ecl;
+extern WARUSR tmpusr;
 
-extern  GALPLNT	planet;
+extern GALSECT sector, *sptr1;
 
-extern  GALWORM	worm;
+extern GALPLNT planet;
 
-extern  PLANETAB	*ptab;
+extern GALWORM worm;
 
-extern  char		map[MAXY][MAXX+1];          /* global scan map array */
-extern  char		mapc[MAXY][MAXX+1];          /* global scan map array color map */
+extern PLANETAB *ptab;
 
-extern  MINE		*mines;       /* place to stuff mines */
+extern char map[MAXY][MAXX + 1];          /* global scan map array */
+extern char mapc[MAXY][MAXX + 1];          /* global scan map array color map */
 
-
-extern  MAIL		mail;
-extern  MAILSTAT			tmpstat;
-
-extern  BEACONTAB	*beacon;
-
-extern  struct message *gemsg;
-
-extern  int			nships;
-extern  int			heading;
-extern  unsigned	speed;
-extern  int			lockwarn;
-extern  int			xsect,ysect;
-extern  unsigned	xcord,ycord, coord2();
-extern  PKEY		pkey;
-extern  unsigned	distance;
-extern  double		ddistance;
-extern  int			bearing;
-extern  unsigned	energy;
-extern  unsigned	damage;
-extern  char		*gechrbuf,*gechrbuf2,*gechrbuf3;
-extern  char 		*warpbuf;
-extern  int			sectnum,plnum;
-extern  GALPLNT	*plptr;
-extern  ITEM		*titems;
-extern  TEAM		*teamtab;
-
-extern  int			su;
-extern  int			tmp_usrnum;
-
-extern  unsigned	plantime;
-
-extern  long		max_plrec,
-						teambonus,
-						pltvcash,
-						pltvdiv,
-						startcash;
-
-extern  int			gemaxplrs,
-						gefreebies,
-						gemaxlist,
-						maxships,
-						se100dam,
-						profon,
-						showopt,
-						syscmds,
-						sysonly,
-						max_plnts,
-						trans_opt,
-						numships,
-						univmax,
-						plodds,
-						wormodds,
-						hpfirdst,
-						hpdammax,
-						pfirdist,
-						pdammax,
-						jamtime,
-						maildays,
-						torpsped,
-						mislsped,
-						decodds,
-						nummines,
-						usermines,
-						maxdroids,
-						cyb_class,
-						clenguse,
-						logflag,
-						optmenu,
-						cyb_gold,
-						tot_classes,
-						team_max,
-						usegemsg,
-						fse_state,
-						phatowrp,
-						misengfc,
-						score_bonus,
-						score_f1,
-						score_f2,
-						chgloser,
-						univwrap,
-						maxplanets;
-
-extern	char		*opttxt,
-						optchr;
-
-extern 	long		*opttbl;
-
-extern	double	tor_fact,
-						tdammax,
-						mis_fact,
-						mdammax,
-						idammax,
-						minedammax,
-						repairrate,
-						tooclose,
-						hyperdist1,
-						hyperdist2,
-						plattrf1,
-						plattrf2,
-						plattrf3,
-						plattrt1,
-						plattrt2,
-						plattrt3;
+extern MINE *mines;       /* place to stuff mines */
 
 
+extern MAIL mail;
+extern MAILSTAT tmpstat;
 
-extern  long	plantock;
+extern BEACONTAB *beacon;
+
+extern struct message *gemsg;
+
+extern int nships;
+extern int heading;
+extern unsigned speed;
+extern int lockwarn;
+extern int xsect, ysect;
+extern unsigned xcord, ycord, coord2();
+extern PKEY pkey;
+extern unsigned distance;
+extern double ddistance;
+extern int bearing;
+extern unsigned energy;
+extern unsigned damage;
+extern char *gechrbuf, *gechrbuf2, *gechrbuf3;
+extern char *warpbuf;
+extern int sectnum, plnum;
+extern GALPLNT *plptr;
+extern ITEM *titems;
+extern TEAM *teamtab;
+
+extern int su;
+extern int tmp_usrnum;
+
+extern unsigned plantime;
+
+extern long max_plrec,
+        teambonus,
+        pltvcash,
+        pltvdiv,
+        startcash;
+
+extern int gemaxplrs,
+        gefreebies,
+        gemaxlist,
+        maxships,
+        se100dam,
+        profon,
+        showopt,
+        syscmds,
+        sysonly,
+        max_plnts,
+        trans_opt,
+        numships,
+        univmax,
+        plodds,
+        wormodds,
+        hpfirdst,
+        hpdammax,
+        pfirdist,
+        pdammax,
+        jamtime,
+        maildays,
+        torpsped,
+        mislsped,
+        decodds,
+        nummines,
+        usermines,
+        maxdroids,
+        cyb_class,
+        clenguse,
+        logflag,
+        optmenu,
+        cyb_gold,
+        tot_classes,
+        team_max,
+        usegemsg,
+        fse_state,
+        phatowrp,
+        misengfc,
+        score_bonus,
+        score_f1,
+        score_f2,
+        chgloser,
+        univwrap,
+        maxplanets;
+
+extern char *opttxt,
+        optchr;
+
+extern long *opttbl;
+
+extern double tor_fact,
+        tdammax,
+        mis_fact,
+        mdammax,
+        idammax,
+        minedammax,
+        repairrate,
+        tooclose,
+        hyperdist1,
+        hyperdist2,
+        plattrf1,
+        plattrf2,
+        plattrf3,
+        plattrt1,
+        plattrt2,
+        plattrt3;
+
+extern long plantock;
 
 #ifdef  TESTDRIVE
 extern  int			endtd,
-						nowtd,
-						lefttd;
+                        nowtd,
+                        lefttd;
 #endif
 
-extern  SHPKEY		shpkey;
-extern  MAILKEY	mailkey;
+extern SHPKEY shpkey;
+extern MAILKEY mailkey;
 
-extern  SHIP		*shipclass;
+extern SHIP *shipclass;
 
-extern  S00			*s00;
-extern  int			s00plnum;
+extern S00 *s00;
+extern int s00plnum;
 
-extern  SCANTAB	*scantab;
+extern SCANTAB *scantab;
 
-extern  long		shieldprice[TOPSHIELD];
-extern  long		phaserprice[TOPPHASOR];
-extern  unsigned	baseprice[NUMITEMS];
-extern  double		maxpl[NUMITEMS];
-extern  long		weight[NUMITEMS];
-extern  long		value[NUMITEMS];
-extern  long	   manhours[NUMITEMS];
+extern long shieldprice[TOPSHIELD];
+extern long phaserprice[TOPPHASOR];
+extern unsigned baseprice[NUMITEMS];
+extern double maxpl[NUMITEMS];
+extern long weight[NUMITEMS];
+extern long value[NUMITEMS];
+extern long manhours[NUMITEMS];
 
 #endif
 
@@ -220,8 +215,8 @@ extern  long	   manhours[NUMITEMS];
 
 #ifndef GECMDS
 
-extern  char		*kwrd[NUMITEMS];
-extern  char		*item_name[NUMITEMS];
+extern char *kwrd[NUMITEMS];
+extern char *item_name[NUMITEMS];
 
 #endif
 
@@ -238,8 +233,8 @@ extern  char		*item_name[NUMITEMS];
 /* GECYBS Global Defs ****************************************************/
 
 #ifndef GECYBS
-extern  char		*cybname;
-extern  int			cybhaltflg;
+extern char *cybname;
+extern int cybhaltflg;
 #endif
 
 #ifdef PHARLAP
@@ -247,21 +242,21 @@ extern  int			cybhaltflg;
 /* MAJORBBS Global Defs **************************************** */
 
 extern
-struct usracc 	*usracc, 	/* user accounting block array               */
-	      		*usacptr;	/* user accounting block pointer for usrnum  */
+struct usracc *usracc,    /* user accounting block array               */
+        *usacptr;    /* user accounting block pointer for usrnum  */
 extern
-int 				usrnum;		/* global user-number (channel) in effect    */
+int usrnum;        /* global user-number (channel) in effect    */
 
 extern
-struct user 	*usrptr, 	/* global pointer to user data in effect     */
-					*user;		/* user volatile-data structure array        */
+struct user *usrptr,    /* global pointer to user data in effect     */
+        *user;        /* user volatile-data structure array        */
 extern
-char input[INPSIZ],   		/* raw user input data buffer                */
-     *margv[INPSIZ/2];		/* array of ptrs to word starts, a la argv[] */
+char input[INPSIZ],        /* raw user input data buffer                */
+        *margv[INPSIZ / 2];        /* array of ptrs to word starts, a la argv[] */
 
 extern
-int margc,						/* number of words in margv[], a la argc     */
-    status;						/* raw status from btusts, where appropriate */
+int margc,                        /* number of words in margv[], a la argc     */
+        status;                        /* raw status from btusts, where appropriate */
 
 
 extern
@@ -269,16 +264,16 @@ struct module *module[NMODS]; /* module access block pointer table         */
 
 
 extern
-int inplen,						/* overall raw input string length           */
-    pfnlvl;						/* profanity level of current input (0-3)    */
+int inplen,                        /* overall raw input string length           */
+        pfnlvl;                        /* profanity level of current input (0-3)    */
 
 extern
-char prfbuf[];					/* 2K buffer used by prfutl, used for staging*/
+char prfbuf[];                    /* 2K buffer used by prfutl, used for staging*/
 
 extern
-struct sysvbl sv;				/* sys-variables record instance for updates */
+struct sysvbl sv;                /* sys-variables record instance for updates */
 
-char *curtim(),*curdat();	/* time and date formatter routines          */
+char *curtim(), *curdat();    /* time and date formatter routines          */
 
 
 extern int othusn;
