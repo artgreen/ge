8c2fe3c052b5c0804a41c39a9004caeaee0061ef
green <arthur@greenjeans.net>
2019-10-27 12:22:15 -0500

    Remove author's note from source code files except gemain.c

--- mbmgeng/gemain.h
+++ mbmgeng/gemain.h
@@ -27,20 +27,6 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA            *
  *************************************************************************/
 
-/**************************************************************************
-* Authors Note: The source code to this program is being made available   *
-*               to the general public in the hopes that it will continue  *
-*               to live on and evolve. While I cannot provide the time    *
-*               to personally participate in its development any longer,  *
-*               I am eager to assist in the design discussions, testing   *
-*               and project discussions.                                  *
-*                                                                         *
-*               To foster a community for Galactic Empire developers I    *
-*               have created a project home page at...                    *
-*               http://mbbs.sitesage.net                                  *
-*                                                                         *
-*               I may be contacted via email at mmurdock@starphire.com    *
-***************************************************************************/
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-declarations"
 #define TRUE            1
